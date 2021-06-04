b343ab4a9a309ae3488bbbe0c2d95aa92b134686
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-06-04 13:37:32 -0400

    minor bug fixes

--- num_spells.py
+++ num_spells.py
@@ -2,13 +2,13 @@ from root_finding import bisection_method
 from func import calculate
 import sys
 
-if len(sys.argv):
+if len(sys.argv) > 1:
     total_life = int(sys.argv[1])
 else:
-    total_life = int(input("How much life do you want to gain"))
+    total_life = int(input("How much life do you want to gain? "))
 
 g = lambda z: calculate(z, 0)[5] - total_life
 
-spells = bisection_method(g, 0, 10)
+spells = bisection_method(g, 0, 100)
 
 print(f"{str(spells):<.10} instant/sorcery spells are required to gain {total_life}")
